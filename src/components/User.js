60be238609282d741d7b5b215e5be548725f19b6
Eduardo Melo <eduardoccmelo@gmail.com>
2021-04-06 21:03:14 +0200

    filter missing

--- src/components/User.js
+++ src/components/User.js
@@ -1,3 +1,13 @@
-function User() {}
+function User({ title, firstName, lastName, gender, picture }) {
+  return (
+    <div className={gender === "male" ? "maleUserBox" : "femaleUserBox"}>
+      <h2>
+        {title}. {firstName} {lastName}
+      </h2>
+      <p>{gender}</p>
+      <img src={picture} alt={picture}></img>
+    </div>
+  );
+}
 
 export default User;
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,3 +1,83 @@
-function UserList() {}
+import { useEffect, useState } from "react";
+import User from "./User";
+
+function UserList() {
+  const [users, setUsers] = useState([]);
+  const [numberOfBoxes, setNumberOfBoxes] = useState();
+  const [filter, setFilter] = useState();
+
+  useEffect(() => {
+    const baseUrl =
+      "https://randomuser.me/api/?inc=email,gender,name,picture&results=100";
+    let url = baseUrl;
+
+    if (numberOfBoxes) {
+      url = `https://randomuser.me/api/?inc=email,gender=${filter},name,picture&results=${numberOfBoxes}`;
+    }
+
+    if (filter) {
+      url = `https://randomuser.me/api/?results=${numberOfBoxes}&gender=${filter}`;
+    }
+
+    console.log(url);
+    console.log(filter);
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data.results);
+      });
+  }, [numberOfBoxes, filter]);
+
+  function renderUsers() {
+    const allUsers = users.map((user) => (
+      <User
+        key={user.email}
+        title={user.name.title}
+        firstName={user.name.first}
+        lastName={user.name.last}
+        gender={user.gender}
+        picture={user.picture.thumbnail}
+      />
+    ));
+    return allUsers;
+  }
+
+  function handleOnChange(event) {
+    const inputElement = event.target;
+    if (inputElement.value === "" || inputElement.value === null) {
+      setNumberOfBoxes("100");
+    } else {
+      setNumberOfBoxes(inputElement.value);
+    }
+  }
+
+  function handleFilter(event) {
+    const inputValue = event.target.value;
+    if (inputValue === "female") {
+      setFilter("female");
+    } else if (inputValue === "male") {
+      setFilter("male");
+    } else if (inputValue === "all") {
+      setFilter("");
+    }
+  }
+
+  return (
+    <div>
+      <label>Number of boxes:</label>
+      <input onChange={handleOnChange}></input>
+      <p>Number of results: {users.length}</p>
+      <div>
+        <select onClick={handleFilter}>
+          <option value="all">All</option>
+          <option value="female">Female</option>
+          <option value="male">Male</option>
+        </select>
+      </div>
+      <div>{renderUsers()}</div>
+    </div>
+  );
+}
 
 export default UserList;
